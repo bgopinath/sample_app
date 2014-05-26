082124ed820d292cf5f70eec18e0b23450058b54
Bharadwaj Gopinath <bgopinath@outlook.com>
2014-05-26 12:01:35 -0700

    Add a full_title helper, pg164

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
+
 describe "StaticPages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
 	describe "Home page" do
 		it "should have the h1 content 'Sample App'" do
@@ -8,9 +11,14 @@ describe "StaticPages" do
 			page.should have_selector('h1', :text => 'Sample App')
 		end
 
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => " | Home")
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => "| Home")
 		end
 	end
 
@@ -22,7 +30,7 @@ describe "StaticPages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => " | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 
@@ -34,15 +42,19 @@ describe "StaticPages" do
 
 		it "should have the title 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => " | About Us")
+			page.should have_selector('title', :text => "#{base_title} | About Us")
 		end
 	end
 
-#  describe "GET /static_pages" do
-#    it "works! (now write some real specs)" do
-#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get static_pages_index_path
-#      response.status.should be(200)
-#    end
-#  end
+	describe "Contact page" do
+		it "should have the h1 content 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', :text => "#{base_title} | Contact")
+		end
+	end
 end
